3b7adb6f593c3563c97c9c9334b91c495faff33b
SHAYAN <135424349+shayanvazirinareh@users.noreply.github.com>
2023-09-29 20:30:32 +0330

    Update index.js

--- index.js
+++ index.js
@@ -10,51 +10,58 @@ let threeModall=['قیچی','کاغذ','سنگ'];
 let testMod;
 testMod=Math.floor(Math.random()*threeModall.length);
 let result=threeModall[testMod]
-function btnclick (e) {
-    e.preventDefault();
+function btnclick () {
     if(text.value==='1'){
         if(result=='کاغذ'){
         p.innerHTML='درست '+result;
         p.style.background='green';
         span.innerHTML++;
+            text.value='';
     }else{
         p.innerHTML='اشتباه '+result;
         p.style.background='red';
         p.style.color='white';
         span.innerHTML--;
+            text.value='';
     }
    }else if(text.value==='2'){
     if(result=='قیچی'){
     p.innerHTML='درست '+result;
     p.style.background='green';
     span.innerHTML++;
+        text.value='';
 }else{
     p.innerHTML='اشتباه '+result;
     p.style.background='red';
     p.style.color='white';
     span.innerHTML--;
+        text.value='';
 }
 }else if(text.value==='3'){
     if(result=='سنگ'){
     p.innerHTML='درست '+result;
     p.style.background='green';
     span.innerHTML++;
+        text.value='';
 }else{
     p.innerHTML='اشتباه '+result;
     p.style.background='red';
     p.style.color='white';
     span.innerHTML--;
+        text.value='';
 }
 }else if(text.value===''){
     p.innerHTML='لطفا فقط عدد وارد وخالی نباشد';
     p.style.background='red';
     p.style.color='white';
     span.innerHTML++;
+        text.value='';
 }else if(text.value<=0||text.value>=4){
     p.innerHTML='فقط عدد بین یک تا سه را وارد کنید';
     p.style.background='red';
     p.style.color='white';
     span.innerHTML--;
+        text.value='';
 }
 }
 btn.addEventListener('click',btnclick)
